7a8237b2a72d4938fea1c390160ef9ca141ce9e2
Zishan Siddique <zishansiddique80@gmail.com>
2023-11-17 18:58:33 +0545

    routes and label for pages added

---
+++ client/src/utils/global_consts.js
@@ -0,0 +1,23 @@
+export const pages=[
+  {
+    id:0,
+    label:"Home",
+    route:"/"
+  },
+  {
+  id:1,
+  label:"Articles",
+  route:"articles"
+},
+  {
+  id:2,
+    label:"Resume",
+    route:"https://drive.google.com/drive/u/0/my-drive",
+  },
+  {
+    id:3,
+    label:"Contact",
+    route:"contact"
+  }
+
+]
