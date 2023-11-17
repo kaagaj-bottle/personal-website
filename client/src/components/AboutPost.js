3caeb7034ff28e4a16ae8ce55e9cb6bf1812ff2d
Zishan Siddique <zishansiddique80@gmail.com>
2023-11-17 21:29:05 +0545

    centered the profile photo

--- client/src/components/AboutPost.js
+++ client/src/components/AboutPost.js
@@ -10,7 +10,7 @@ import aboutPagePicture from '../../asset/images/aboutPagePicture.png'
 
 const AboutPost = () => {
   const [aboutPost, setAboutPost] = useState('')
- 
+
   // this is just a temporary solution. the md file will probably be stored as some form of text in database which will be parsed for rendering in frontend
   useEffect(() => {
     import('../../asset/markdown/aboutPage.md')
@@ -27,13 +27,23 @@ const AboutPost = () => {
         display: { sm: 'flex' },
       }}>
         <Box
-          component="img"
-          src={aboutPagePicture} alt="my profile picture"
+          component='div'
           sx={{
-            mx: 'auto',
-            maxHeight: { xs: 350, sm: 200 }
+
+            display: 'flex',
+            justifyContent: 'center',
           }}
-        />
+        >
+          <Box
+            component='img'
+            src={aboutPagePicture} alt='my profile picture'
+            sx={{
+              display: 'flex',
+              maxHeight: { xs: 350, sm: 200 }
+            }}
+          />
+        </Box>
+
         <Box component='div' sx={{
           px: 5,
           fontSize: '0.9rem'
